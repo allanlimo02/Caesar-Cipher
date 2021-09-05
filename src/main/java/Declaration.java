4f528f4acf06172d9d852e670239eeddf181b581
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-05 15:03:35 +0300

    Created and modifed Declaration.java file

---
+++ src/main/java/Declaration.java
@@ -0,0 +1,14 @@
+public class Declaration {
+
+    private String alpha="abcdefghijklmnopqrstuvwxyz";
+
+
+    public String getAlpha() {
+        return alpha;
+    };
+
+    public void setAlpha() {
+
+        this.alpha = alpha;
+    }
+}
