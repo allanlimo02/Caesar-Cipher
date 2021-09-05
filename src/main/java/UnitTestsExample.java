800bdd9a6dc1d07f61e655067beb210557b20a70
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-05 15:05:15 +0300

    Created and modifed UnitTestExample.java file

---
+++ src/main/java/UnitTestsExample.java
@@ -0,0 +1,11 @@
+public class UnitTestsExample {
+
+    public int addition(int a,int b){
+
+        return a+b;
+    }
+    public String concationateString(String a,String b){
+
+        return a.concat(b);
+    }
+}
