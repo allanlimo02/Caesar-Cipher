20f614b1314f837bc7c6d8c6b85080dadcf564f7
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-06 13:02:42 +0300

    Done tests

--- src/test/java/CipherTest.java
+++ src/test/java/CipherTest.java
@@ -3,23 +3,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class CipherTest {
-
-
     @Test
-    void encodeMethod() {
+    void transformBeforeEncoding() {
         Cipher cipher = new Cipher();
-        assertEquals("fqqfs",cipher.decode("allan",5));
+        assertEquals("fqqfs",cipher.transformBeforeEncoding("allan",5));
     }
 
     @Test
-    void decodeMethod() {
+    void encode() {
+        Cipher cipher = new Cipher();
+        assertEquals("fqqfs",cipher.encode("allan",5));
+    }
+
+    @Test
+    void decode() {
         Cipher cipher = new Cipher();
         assertEquals("allan",cipher.decode("fqqfs",5));
     }
 
-    @Test
-    void encodeFinalTrial() {
-        Cipher cipher = new Cipher();
-        assertEquals("Allan Limo",cipher.decode("Fqqfs%Qnrt",5));
-    }
+
+//    @Test
+//    void encodeMethod() {
+//        Cipher cipher = new Cipher();
+//        assertEquals("fqqfs",cipher.decode("allan",5));
+//    }
+//
+//    @Test
+//    void decodeMethod() {
+//        Cipher cipher = new Cipher();
+//        assertEquals("allan",cipher.decode("fqqfs",5));
+//    }
+//
+//    @Test
+//    void encodeFinalTrial() {
+//        Cipher cipher = new Cipher();
+//        assertEquals("Allan Limo",cipher.decode("Fqqfs%Qnrt",5));
+//    }
 }
