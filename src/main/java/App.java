8bcfc9cc261cdb1c0b3c6ed071bd9efeaa7c8931
allanlimo02 <allan.limo@student.moringaschool.com>
2021-09-06 11:59:09 +0300

    Adde method acces

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -19,11 +19,9 @@ public class App {
                     }else{
                         System.out.println("Great! Now Input text to be encrypted");
                         String tobeEncoded = scan.next();
-                        String encodedFinal= Cipher.Encode(tobeEncoded, encryptingKey);
+                        String encodedFinal= new Cipher().Encode(tobeEncoded, encryptingKey);
                         System.out.println("Your encoded Message is >> \n"+encodedFinal);
                     }
-
-
             } else if (optionMain == 2) {
                 System.out.println("Awesome! now input your encryption key");
                 int decryptingKey = scan.nextInt();
@@ -34,7 +32,7 @@ public class App {
                     String tobeDecoded = scan.next();
 
 
-                    String decodedFinal=Cipher.decode(tobeDecoded,decryptingKey);
+                    String decodedFinal=new Cipher().decode(tobeDecoded,decryptingKey);
                     System.out.println("Your decoded Message is \n >> "+decodedFinal);
 
                 }
